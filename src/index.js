6dc7775c178161608ac1fe939f79886bf647a7dd
Huang Zhe <3451701311@qq.com>
2024-02-03 20:07:14 +0800

    feat: first init. 完成一个简单的 Vue响应式系统.

---
+++ src/index.js
@@ -0,0 +1,70 @@
+let activeEffect;
+let bucket = new WeakMap();
+
+/**
+ * activity bucket trigger
+ * @param target
+ * @param key
+ * @param newValue
+ */
+function trigger(target, key, newValue) {
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    return;
+  }
+
+  const effectsFn = depsMap.get(key);
+  if (!effectsFn) {
+    return;
+  }
+  effectsFn.forEach((fn) => {
+    fn();
+  });
+}
+
+/**
+ * activity bucket add track
+ * @param target
+ * @param key
+ */
+function track(target, key) {
+  if (!activeEffect) {
+    return;
+  }
+  let depsMap = bucket.get(target);
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  let deps = bucket.get(depsMap);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
+  }
+  deps.add(activeEffect);
+}
+
+const objProxy = new Proxy(
+  { ok: true },
+  {
+    set(target, p, newValue, receiver) {
+      const result = Reflect.set(target, p, newValue, receiver);
+      trigger(target, p, newValue);
+      return result;
+    },
+    get(target, p, receiver) {
+      track(target, p);
+      return Reflect.get(target, p, receiver);
+    },
+  },
+);
+
+function effect(fn) {
+  activeEffect = fn;
+  fn();
+}
+
+effect(() => {
+  console.log("the objProxy.ok value has change. -->", objProxy.ok);
+});
+setTimeout(() => {
+  objProxy.ok = false;
+}, 3000);
